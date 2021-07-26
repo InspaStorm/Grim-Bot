295a98ae08c643611dda77fd79a8caa17b359729
Ranger <yemzero0@gmail.com>
2021-07-26 08:00:58 +0530

    📃 Added task command

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-const {token} = require('./config.js')
+const {token} = require('./config.js');
 const discord = require('discord.js');
-const keys = require('./token.json')
-const {keepAlive} = require('./server.js')
+const {keepAlive} = require('./server.js');
+const fs = require('fs');
+const tasks = require('./tasks.json')
 
 const client = new discord.Client();
 
+client.on('ready' , () => console.log('Grim Bot logged on!'))
+
 client.on('message', msg => {
 	const lowerCasedMsg = msg.content.toLowerCase()
-
 	if (lowerCasedMsg == 'task') {
-		msg.channel.send('No tasks!')
+		const authorID = msg.author.id
+		if (tasks.userIds.includes(msg.author.id)) {
+
+			try {
+
+				const tasksList = tasks[authorID].tasks.join('\n');
+				const name = tasks[authorID].name;
+
+				const tasksEmbed = new discord.MessageEmbed()
+					.setTitle(`Tasks for ${name}`)
+					.setDescription('Tasks assigned to YOU!')
+					.addField('Tasks', tasksList)
+					.setThumbnail(msg.author.avatarURL())
+
+				msg.channel.send(tasksEmbed)
+			} catch (err) {
+
+				console.error(err)
+				msg.channel.send('Something went wrong on my side =/')
+
+			}
+
+		}
 	}
 })
 
