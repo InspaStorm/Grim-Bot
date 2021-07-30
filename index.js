7d7d473c297141fad95e164fe302fac622ef0eae
Ranger <yemzero0@gmail.com>
2021-07-30 07:57:04 +0530

    📂 Added a new way to sort out commands

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {token} = require('./config.js');
 const discord = require('discord.js');
 const {keepAlive} = require('./server.js');
 const fs = require('fs');
-const tasks = require('./tasks.json')
+const tasks = require('./tasks.json');
 
 const {prefix} = require('./config.js')
 const client = new discord.Client();
@@ -11,7 +11,7 @@ client.on('ready' , () => {
 
 	client.user.setPresence({
         	activity: {
-        		name: 'Grim Town',
+        		name: 'Grim Town | g!help',
         		type: "PLAYING",
         	},
         	status: 'idle'
@@ -19,6 +19,22 @@ client.on('ready' , () => {
 		.then(console.log(`${client.user.tag} logged on!`))
 		.catch(err => console.log(err))
 });
+
+const commandFiles = fs.readdirSync('./commands')
+
+client.commands = new discord.Collection();
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`)
+
+	client.commands.set(command.name, command)
+}
+
+let commands = []
+
+for (const command of client.commands) {
+	commands.push({name: command[1].name, value: command[1].description})
+}
 
 client.on('message', msg => {
 	const lowerCasedMsg = msg.content.toLowerCase()
@@ -29,39 +45,12 @@ client.on('message', msg => {
 
 		const command = lowerCasedMsg.substr(2)
 
-		if (command == 'task') {
-
-			const authorID = msg.author.id
-
-			if (tasks.userIds.includes(msg.author.id)) {
-
-				try {
-
-					const tasksList = tasks[authorID].tasks.join('\n');
-					const name = tasks[authorID].name;
-
-					const tasksEmbed = new discord.MessageEmbed()
-						.setTitle(`Tasks for ${name}`)
-						.setDescription('Tasks assigned to YOU!')
-						.addField('Tasks', tasksList)
-						.setThumbnail(msg.author.avatarURL())
-
-					msg.channel.send(tasksEmbed)
-				} catch (err) {
-
-					console.error(err)
-					msg.channel.send('Something went wrong on my side =/')
-
-				}
-
+		if (client.commands.has(command)) {
+			try {
+				client.commands.get(command).run(msg)
+			} catch (err) {
+				console.log(`Something went wrong executing command: ${err}`)
 			}
-		}
-
-		else if (command == 'myavatar') {
-			const avatarEmbed = new discord.MessageEmbed()
-			.setImage(msg.author.avatarURL());
-
-			msg.channel.send(avatarEmbed)
 		}
 		
 		else if (command == 'ping') {
@@ -73,20 +62,13 @@ client.on('message', msg => {
 			    .setColor('#00ffff')
 			    .setTitle('Commands')
 			    .setDescription('You can see the commands of GRIM BOT here')
-			    .addFields(
-				{ name: '\u200B', value: '\u200B' },
-				{ name: 'Help', value: 'You can use this command to see all the commands ;p' },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: 'Hello', value: 'Say hello to bot for it to respond back so your never lonely'},
-				{ name: 'Ping', value: 'Pong'},
-				{ name: 'Task', value: 'You know if you know'},
-			    )
+			    .addFields(commands)
 			    .setImage('https://thumbs.dreamstime.com/z/help-11277.jpg')
 			    .setFooter('Developed by @DeadlineBoss & @Ranger');
 			
 			msg.channel.send(helpEmbed);
 		}
-	} else if (lowerCasedMsg.match(/hi*|hello*|hey*/)) {
+	} else if (lowerCasedMsg.startsWith('hm')) {
 		const randInt = Math.floor(Math.random() * 5)
 		const greetBack = lowerCasedMsg.slice(0, -1) + (lowerCasedMsg.substr(-1).repeat(randInt))
 		msg.reply(greetBack);
