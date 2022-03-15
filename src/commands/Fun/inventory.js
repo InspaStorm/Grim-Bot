69aa1877cd447e142806d5756d8655e9499bd9d7
Ranger <yemzero0@gmail.com>
2022-03-15 22:22:41 +0530

    🛒 Added shop system

---
+++ src/commands/Fun/inventory.js
@@ -0,0 +1,68 @@
+import dbManager from '../../helpers/dbCrud.js';
+import Table from 'text-table';
+
+
+const inv = new dbManager('inventory')
+
+function alteredLook(e) {
+	const alias = {chm: "✉️ Custom Hmm Message Token",}
+
+	if (Object.keys(alias).includes(e)) {
+		return alias[e]
+	}
+
+	return e
+}
+
+export default {
+
+	name: 'inventory',
+	description:'Preview the chad chatters',
+	alias: ['e', 'pocket'],
+	options: [],
+
+	async run(msg, args, author = msg.author, isInteraction = false) {
+        const user =author
+        const items = await inv.singleFind({id: user.id})
+
+		const config = {
+			border: {
+			  topBody: `─`,
+			  topJoin: `┬`,
+			  topLeft: `┌`,
+			  topRight: `┐`,
+		  
+			  bottomBody: `─`,
+			  bottomJoin: `┴`,
+			  bottomLeft: `└`,
+			  bottomRight: `┘`,
+		  
+			  bodyLeft: `│`,
+			  bodyRight: `│`,
+			  bodyJoin: `│`,
+		  
+			  joinBody: `─`,
+			  joinLeft: `├`,
+			  joinRight: `┤`,
+			  joinJoin: `┼`
+			}
+		  };
+		
+		// [`**${user.username}'s Inventory:**\n\n`]
+		let list = [];
+		list.push([`\`\`\`${user.username}'s Inventory:\`\`\``])
+        
+        if (items != null) {
+            ['id', '_id'].forEach(key => delete items[key])
+
+            Object.keys(items).forEach(e => list.push([`> **${alteredLook(e)}**`, `${items[e]}`]));
+
+			list.push(['\u200b'], ['*NB: This feature is not yet completely usable =(*'])
+        } else list += '**Empty Inventory**'
+
+		const data = Table(list, {hsep: '  **-**  '})
+
+        return {content: data}
+	}
+
+}
--- src/helpers/dbCrud.js
+++ src/helpers/dbCrud.js
@@ -1,33 +1,37 @@
 import {db} from '../startup/database.js'
 
-export async function singleFind(collectionName, query) {
-    const collection = db.collection(collectionName)
-
-    const res = await collection.findOne(query)
-
-    return res;
-}
-
-export async function multiFind(collectionName, query, returnArr = true) {
-    const collection = db.collection(collectionName)
-
-    const res = await collection.find(query)
-
-    if (returnArr) {
-        const arr = res.toArray()
-        return arr;
+export default class dbManager {
+    constructor(name) {
+        this.collection = db.collection(name)
     }
-    return res;
+    
+    async singleFind(query) {
+      
+        const res = await this.collection.findOne(query)
+    
+        return res;
+    }
+    
+    async multiFind(query, returnArr = true) {
+      
+        const res = await this.collection.find(query)
+    
+        if (returnArr) {
+            const arr = res.toArray()
+            return arr;
+        }
+        return res;
+    }
+    
+    async singleUpdate(query, updateValue) {
+      
+        await this.collection.updateOne(query, updateValue)
+    }
+    
+    async singleInsert(insertValue) {
+      
+        await this.collection.insertOne(insertValue)
+    }
+
 }
 
-export async function singleUpdate(collectionName, query, updateValue) {
-    const collection = db.collection(collectionName)
-
-    await collection.updateOne(query, updateValue)
-}
-
-export async function singleInsert(collectionName, insertValue) {
-    const collection = db.collection(collectionName)
-
-    await collection.insertOne(insertValue)
-}
