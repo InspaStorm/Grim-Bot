d24ee6499d4da3a932460c30beb875cb2d641f31
Ranger <yemzero0@gmail.com>
2022-06-04 12:39:35 +0530

    🕹️ Made GameManager and other related things into a separate file

--- src/helpers/apiResolver.js
+++ src/helpers/apiResolver.js
@@ -6,7 +6,7 @@ import { Message } from "discord.js";
  * @param {Message} destination The message/interaction to reply to
  * @param {Object} content The message/thing to be sent
  * @param {Boolean} isInteraction 
- * @returns {Message}
+ * @returns {Message} Message object that was sent
  */
 export async function replier(destination, content, isInteraction = false) {
     if (isInteraction) content.fetchReply = true
@@ -25,6 +25,13 @@ export async function sender(destination, content) {
     return messageInfo
 }
 
+/**
+ * 
+ * @param {Message} destination 
+ * @param {*} content 
+ * @param {boolean} isInteraction 
+ * @returns 
+ */
 export async function followUp(destination, content, isInteraction) {
     const msg = await destination.edit(content)
     return msg
